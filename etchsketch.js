da9a63800955e430588de8c69d3c33b16d8e0d6d
Kristof Kulcsar <rsrlz94@gmail.com>
2015-08-01 18:31:52 +0200

    almost final build

--- etchsketch.js
+++ etchsketch.js
@@ -1,12 +1,20 @@
+var pencolor = "red";
 $(document).ready(function(){
 	addSquares(16);
 
-	$(document).on('mouseover','.square',function(){
-		$(this).css('background-color','yellow');
+	$('#wrapper').on('mousedown',function(){
+			$('#wrapper').on('mouseover','.square',function(){
+				$(this).css('background-color',pencolor);
+			});
+
+	});
+
+	$('input[name=clear]').click(function(){
+		$('.square').css('background-color','#ddd');
 	});
 
 	$('input[name=reset]').click(function(){
-		$('.square').hide();
+		$('.square').remove();
 
 		value = prompt("How many squares per line you want fam?\n(Don't say just fuck my shit up)");
 
@@ -19,8 +27,7 @@ $(document).ready(function(){
 
 function addSquares(value)
 {
-	value*=value;
-	for(i = 0; i < value;i++)
+	for(i = 0; i < value*value;i++)
 	{
 		$('#wrapper').append('<div class="square"></div>');
 	}
